8dd6772abf3a61ca06e71a4d53b4323333144d4c
wangjilong <yudi010@126.com>
2019-03-11 17:56:03 +0900

    新しい作成

---
+++ bootsample/src/main/java/com/bootsample/BootsampleApplication.java
@@ -0,0 +1,13 @@
+package com.bootsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootsampleApplication.class, args);
+	}
+
+}
