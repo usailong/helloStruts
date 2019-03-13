3c41124826c828e08f113f2e2f15d3c90c1c94ba
yudi010 <yudi010@126.com>
2019-03-13 17:28:45 +0900

    main file add

---
+++ bootsample/src/test/java/com/bootsample/TestMain.java
@@ -0,0 +1,11 @@
+package com.bootsample;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+// javac から呼び出すときに、一番目の引数はJavaとなります。
+		//System.out.println(args[0]);
+	}
+
+}
